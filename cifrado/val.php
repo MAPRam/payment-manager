9652fa2478b6faf624f2eb4db32b01646c409d6a
Marco Antonio Pascual Ramírez <matoz.psc@gmail.com>
2019-06-28 10:14:44 -0500

    Métodos de cifrado

---
+++ cifrado/val.php
@@ -0,0 +1,33 @@
+<?php
+
+  function key()
+  {
+      $key = array('0' => 224,'1' => 210,'2' => 244,'3' => 244,'4' => 194,'5' => 224,'6' => 222,'7' => 216,'8' => 216 );
+      $k = '';
+
+     /*
+		CICLO DE DESCIFRADO
+		OCULTO POR SEGURIDAD
+	*/
+
+      return $k;
+  }
+
+
+
+  function cifra($cadena, $llave)
+  {
+     $result = '';
+     
+	/*
+
+	PROCEDIMIENTOS DE CIFRADO/DESCRIFRADO
+	OCULTO POR SEGURIDAD	
+
+	*/
+
+     return base64_encode($result);
+  }
+
+
+?>
